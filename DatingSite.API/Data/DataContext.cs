6729844a8d89e5f66337c123504092f576575226
mariogonzal <mario_agh@hotmail.com>
2019-12-01 21:59:02 +0100

    EntityFramework Database Init

---
+++ DatingSite.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using DatingSite.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingSite.API.Data
+{
+    public class DataContext:DbContext
+    {
+        
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values  { get; set; }
+    }
+}
