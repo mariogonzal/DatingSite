a2f6883a74840de9d6ededc0f3866e47eaaa85f4
mariogonzal <mario_agh@hotmail.com>
2019-12-06 23:29:05 +0100

    extended user entity migration

--- DatingSite.API/Models/User.cs
+++ DatingSite.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace DatingSite.API.Models
 {
     public class User:DataEntityModelBase
@@ -8,5 +11,18 @@ namespace DatingSite.API.Models
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfBird { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interest { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
